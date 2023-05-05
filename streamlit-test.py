02696c0bd37cababfc1f6158313c91d2c73aca54
Caspar657 <81959728+Caspar657@users.noreply.github.com>
2023-05-05 13:54:53 +1000

    Update streamlit-test.py

--- streamlit-test.py
+++ streamlit-test.py
@@ -84,32 +84,6 @@ def share_of_search(kw_list, start_date, end_date, geography):
 
             def pytrends_query(kw_list, start_date, end_date):
                 from pytrends.request import TrendReq
-                #adding request headers to bypass Google
-                request_args = {"authority": "trends.google.com",
-                    "method": "GET",
-                    "path": "/trends/api/autocomplete/rupert?hl=en-US&tz=-660",
-                    "scheme": "https",
-                    "accept": "application/json, text/plain, */*",
-                    "accept-encoding": "gzip, deflate, br",
-                    "accept-language": "en-US,en;q=0.9",
-                    "cookie": "OGPC=19033657-1:19033886-1:19033941-1:; SEARCH_SAMESITE=CgQI3ZcB; OTZ=6940735_12_12_133440_16_395520; NID=511=k8bZtTtj3MLjSlrtSuErN5sXHLLCrzRh8ww1AW413sUGhFzgrtscuMmVfAH-NTIlwuI6KX11uK5G0GqmfcjJEkRt65aKknhyO8T3A8bBNb9N12BCjcoDPcdXbVQc3Yo_n4Pd3aAdgk3MV2OPBXIpjzui5auH-lBi9HBgdXBQCEUkK3Z_RtmZQVcz69jb0pCZ4-3W0OhnzCMyUPogEcq8Jazzq0ULoqTg2KlR31lcP7Br2gtnrPA-m7dNeKorUcjfIF9PwNIrB8hhwb8sOglpWVqPZJnfEavVJ7cgp0SLvDB46JHgk5lQO7P4Z6r1WKJKz37jmPw9-IY8ADHzXRyP8yG1; S=billing-ui-v3=JiJd58DXAy-29bylAEET7qUfAfds9_nH:billing-ui-v3-efe=JiJd58DXAy-29bylAEET7qUfAfds9_nH; SID=UgjnaG0JIOZXWjL4EUjpVEk7L4z3OtD9pZAnG7lv0at7kWAIxKvmqVA-ESY9ApGDwJUjkw.; __Secure-1PSID=UgjnaG0JIOZXWjL4EUjpVEk7L4z3OtD9pZAnG7lv0at7kWAIbLwZDOcy8q_K-yyMaLiMHw.; __Secure-3PSID=UgjnaG0JIOZXWjL4EUjpVEk7L4z3OtD9pZAnG7lv0at7kWAIN2uljmpTUHedN6O4u8UxCQ.; HSID=Abb0FHJ9Rc-fZ2I1U; SSID=AgF4urAnjOE7YPMoi; APISID=vUS0443UOwyKTulF/A-OLsCVykRhDa6HLA; SAPISID=22BPlBATZGbzOkMy/ANxbkj4Gf55CI6GVx; __Secure-1PAPISID=22BPlBATZGbzOkMy/ANxbkj4Gf55CI6GVx; __Secure-3PAPISID=22BPlBATZGbzOkMy/ANxbkj4Gf55CI6GVx; AEC=ARSKqsJ4tRzLHjth73H72eKw_ZyffGFlORW8ZDfCgR8Akochwb7wQlAkt6U; 1P_JAR=2023-03-22-02; SIDCC=AFvIBn-J7TGd0Au_NGsYrsuFNJE5KA4rd9q8JB4U36-FmoKIqEJ9mfCddwXamiSMJwxQ1zLaZKM; __Secure-1PSIDCC=AFvIBn8EmRXfV0BxmxpbrtknrU8-iur5ZgRyyM9Q_9cXR1OMNBSmC8Mc-CR2JjHZoUMinW_hnwI; __Secure-3PSIDCC=AFvIBn9r4E_WoRGpF8hJkdboQlzy25O64lfC2mqVA5gq8KXLh5NvhH2JDYPX9OTFrjG3UT1De7o",
-                    "referer": "https://trends.google.com/trends/explore?q=Trump&date=now%201-d&geo=AU&hl=en",
-                    "sec-ch-ua": '"Chromium";v="106", "Google Chrome";v="106", "Not;A=Brand";v="99"',
-                    "sec-ch-ua-arch": "arm",
-                    "sec-ch-ua-bitness": "64",
-                    "sec-ch-ua-full-version": "106.0.5249.119",
-                    "sec-ch-ua-full-version-list": '"Chromium";v="106.0.5249.119", "Google Chrome";v="106.0.5249.119", "Not;A=Brand";v="99.0.0.0"',
-                    "sec-ch-ua-mobile": "?0",
-                    "sec-ch-ua-platform": "macOS",
-                    "sec-ch-ua-platform-version": "12.5.1",
-                    "sec-ch-ua-wow64": "?0",
-                    "sec-fetch-dest": "empty",
-                    "sec-fetch-mode": "cors",
-                    "sec-fetch-site": "same-origin",
-                    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
-                    "x-client-data": "CI62yQEIprbJAQjBtskBCKmdygEIgPzKAQiSocsBCPyqzAEI5JfNAQ=="
-                    }
-                pytrends = TrendReq(hl='en-US', tz = 600, request_args = request_args)
                 pytrends.build_payload(kw_list, cat=0, timeframe=start_date + " " + end_date, geo=geography, gprop='')
                 
                 df = pytrends.interest_over_time()
